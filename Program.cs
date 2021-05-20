3012e4308f5a3bc673ef6caf0f89f0548e93639b
TakesureM <tiripamwepo@gmail.com>
2021-05-20 18:49:04 +0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+      static void Main(string[] args)
+      {
+          Console.WriteLine("Hello World");
+      }
+    }
+}
